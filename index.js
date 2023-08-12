f37b874f2d81615bbf75fbdf55c04458fbf70395
sunrisean9el <kasztan7331@gmail.com>
2023-08-12 11:15:14 +0200

    Fixes
    -Fix edit function
    -Fix save function

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const expenseBtn = document.querySelector("#expense-btn");
 const expensesList = document.querySelector("#expenses-list");
 const expensesSum = document.querySelector("#expenses-sum");
 
-const list = [];
+const listOfIncomes = [];
+const listOfExpenses = [];
 let balance = 0;
 let totalIncomes = 0;
 let totalExpenses = 0;
@@ -23,6 +24,7 @@ const addSaveBtn = () => {
   const saveBtn = document.createElement("button");
   saveBtn.textContent = "Zapisz";
   saveBtn.classList = "save-btn";
+  saveBtn.hidden = true;
   return saveBtn;
 };
 
@@ -61,21 +63,33 @@ const updateBalance = () => {
 
 const addIncome = () => {
   let i = 0;
-  if (incomeName.value != "" && incomeAmount.value > 0) {
+  if (incomeName.value && incomeAmount.value > 0) {
     const newItem = document.createElement("li");
-    if (list.length != 0) {
-      i = list[list.length - 1].id + 1;
+    if (listOfIncomes.length != 0) {
+      i = listOfIncomes[listOfIncomes.length - 1].id + 1;
     }
 
     newItem.id = i;
-    newItem.textContent = `${incomeName.value}: ${incomeAmount.value} zł`;
     newItem.classList = "new-item";
     incomesList.appendChild(newItem);
+
+    const spanElemName = document.createElement("span");
+    spanElemName.classList = "income-name";
+    newItem.appendChild(spanElemName);
+    const spanElemValue = document.createElement("span");
+    spanElemValue.classList = "income-value";
+    newItem.appendChild(spanElemValue);
+
+    spanElemName.textContent = `${incomeName.value}`;
+    spanElemValue.textContent = `${incomeAmount.value}`;
+
     const btnDiv = document.createElement("div");
     btnDiv.classList = "btn-div";
     newItem.appendChild(btnDiv);
     const editBtn = addEditBtn();
     btnDiv.appendChild(editBtn);
+    const saveBtn = addSaveBtn();
+    btnDiv.appendChild(saveBtn);
     const deleteBtn = addDeleteBtn();
     btnDiv.appendChild(deleteBtn);
 
@@ -88,38 +102,83 @@ const addIncome = () => {
     totalIncomes += incomeAmount.valueAsNumber;
 
     editBtn.addEventListener("click", (event) => {
-      const target = event.target;
-      const btnDiv = target.parentNode;
-      const li = btnDiv.parentNode;
-      const itemToEdit = list.findIndex((object) => object.id == li.id);
-      incomeName.value = list[itemToEdit].name;
-      incomeAmount.value = list[itemToEdit].value;
-      totalIncomes -= list[itemToEdit].value;
-      list.splice(itemToEdit, 1);
-      li.parentElement.removeChild(li);
-      const saveBtn = addSaveBtn();
-      incomeBtn.replaceWith(saveBtn);
-      incomesSum.textContent = `${totalIncomes}`;
+      const button = event.target;
+      const li = button.parentNode.parentNode;
 
-      saveBtn.addEventListener("click", () => {
-        addIncome();
-        saveBtn.replaceWith(incomeBtn);
-      });
+      const spanElemName = li.children[0];
+      const editNameInput = document.createElement("input");
+      editNameInput.value = spanElemName.textContent;
+      editNameInput.classList = "edit-input";
+
+      li.insertBefore(editNameInput, spanElemName);
+      spanElemName.remove();
+
+      const spanElemValue = li.children[1];
+      const editValueInput = document.createElement("input");
+      editValueInput.type = "number";
+      editValueInput.value = spanElemValue.textContent;
+      editValueInput.classList = "edit-input";
+
+      li.insertBefore(editValueInput, spanElemValue);
+      spanElemValue.remove();
+
+      saveBtn.hidden = false;
+      editBtn.hidden = true;
+    });
+
+    saveBtn.addEventListener("click", (event) => {
+      const button = event.target;
+      const li = button.parentNode.parentNode;
+
+      const editNameInput = li.children[0];
+
+      const editValueInput = li.children[1];
+      const editValue = Number(editValueInput.value);
+
+      if (editNameInput.value && editValue && editValue > 0) {
+        const spanElemName = document.createElement("span");
+        spanElemName.classList = "income-name";
+        spanElemName.textContent = editNameInput.value;
+        li.insertBefore(spanElemName, editNameInput);
+        editNameInput.remove();
+
+        const spanElemValue = document.createElement("span");
+        spanElemValue.classList = "income-value";
+        spanElemValue.textContent = editValueInput.value;
+        li.insertBefore(spanElemValue, editValueInput);
+        editValueInput.remove();
+
+        totalIncomes -= Number(income.value);
+        totalIncomes += editValueInput.valueAsNumber;
+
+        updateBalance();
+        incomesSum.textContent = `${totalIncomes}`;
+
+        income.name = spanElemName.textContent;
+        income.value = spanElemValue.textContent;
+
+        saveBtn.hidden = true;
+        editBtn.hidden = false;
+      } else {
+        alert("Wprowadź poprawne dane");
+      }
     });
 
     deleteBtn.addEventListener("click", (event) => {
       const target = event.target;
       const btnDiv = target.parentNode;
       const li = btnDiv.parentNode;
-      const itemToDelete = list.findIndex((object) => object.id == li.id);
-      totalIncomes -= list[itemToDelete].value;
-      list.splice(itemToDelete, 1);
-      li.parentElement.removeChild(li);
+      const itemToDelete = listOfIncomes.findIndex(
+        (object) => object.id == li.id
+      );
+      totalIncomes -= listOfIncomes[itemToDelete].value;
+      listOfIncomes.splice(itemToDelete, 1);
+      li.remove();
       updateBalance();
       incomesSum.textContent = `${totalIncomes}`;
     });
 
-    list.push(income);
+    listOfIncomes.push(income);
     incomeName.value = "";
     incomeAmount.value = "";
     incomesSum.textContent = `${totalIncomes}`;
@@ -135,21 +194,33 @@ incomeBtn.addEventListener("click", addIncome);
 
 const addExpense = () => {
   let i = 0;
-  if (expenseName.value && expenseAmount.value) {
+  if (expenseName.value && expenseAmount.value > 0) {
     const newItem = document.createElement("li");
-    if (list.length != 0) {
-      i = list[list.length - 1].id + 1;
+    if (listOfExpenses.length != 0) {
+      i = listOfExpenses[listOfExpenses.length - 1].id + 1;
     }
 
     newItem.id = i;
-    newItem.textContent = `${expenseName.value}: ${expenseAmount.value} zł`;
     newItem.classList = "new-item";
     expensesList.appendChild(newItem);
+
+    const spanElemName = document.createElement("span");
+    spanElemName.classList = "li-name";
+    newItem.appendChild(spanElemName);
+    const spanElemValue = document.createElement("span");
+    spanElemValue.classList = "li-value";
+    newItem.appendChild(spanElemValue);
+
+    spanElemName.textContent = `${expenseName.value}`;
+    spanElemValue.textContent = `${expenseAmount.value}`;
+
     const btnDiv = document.createElement("div");
     btnDiv.classList = "btn-div";
     newItem.appendChild(btnDiv);
     const editBtn = addEditBtn();
     btnDiv.appendChild(editBtn);
+    const saveBtn = addSaveBtn();
+    btnDiv.appendChild(saveBtn);
     const deleteBtn = addDeleteBtn();
     btnDiv.appendChild(deleteBtn);
 
@@ -162,38 +233,83 @@ const addExpense = () => {
     totalExpenses += expenseAmount.valueAsNumber;
 
     editBtn.addEventListener("click", (event) => {
-      const target = event.target;
-      const btnDiv = target.parentNode;
-      const li = btnDiv.parentNode;
-      const itemToEdit = list.findIndex((object) => object.id == li.id);
-      expenseName.value = list[itemToEdit].name;
-      expenseAmount.value = list[itemToEdit].value;
-      totalExpenses -= list[itemToEdit].value;
-      list.splice(itemToEdit, 1);
-      li.parentElement.removeChild(li);
-      const saveBtn = addSaveBtn();
-      expenseBtn.replaceWith(saveBtn);
-      expensesSum.textContent = `${totalExpenses}`;
+      const button = event.target;
+      const li = button.parentNode.parentNode;
 
-      saveBtn.addEventListener("click", () => {
-        addExpense();
-        saveBtn.replaceWith(expenseBtn);
-      });
+      const spanElemName = li.children[0];
+      const editNameInput = document.createElement("input");
+      editNameInput.value = spanElemName.textContent;
+      editNameInput.classList = "edit-input";
+
+      li.insertBefore(editNameInput, spanElemName);
+      spanElemName.remove();
+
+      const spanElemValue = li.children[1];
+      const editValueInput = document.createElement("input");
+      editValueInput.type = "number";
+      editValueInput.value = spanElemValue.textContent;
+      editValueInput.classList = "edit-input";
+
+      li.insertBefore(editValueInput, spanElemValue);
+      spanElemValue.remove();
+
+      saveBtn.hidden = false;
+      editBtn.hidden = true;
+    });
+
+    saveBtn.addEventListener("click", (event) => {
+      const button = event.target;
+      const li = button.parentNode.parentNode;
+
+      const editNameInput = li.children[0];
+
+      const editValueInput = li.children[1];
+      const editValue = Number(editValueInput.value);
+
+      if (editNameInput.value && editValue && editValue > 0) {
+        const spanElemName = document.createElement("span");
+        spanElemName.classList = "li-name";
+        spanElemName.textContent = editNameInput.value;
+        li.insertBefore(spanElemName, editNameInput);
+        editNameInput.remove();
+
+        const spanElemValue = document.createElement("span");
+        spanElemValue.classList = "li-value";
+        spanElemValue.textContent = editValueInput.value;
+        li.insertBefore(spanElemValue, editValueInput);
+        editValueInput.remove();
+
+        totalExpenses -= Number(expense.value);
+        totalExpenses += editValueInput.valueAsNumber;
+
+        updateBalance();
+        expensesSum.textContent = `${totalExpenses}`;
+
+        expense.name = spanElemName.textContent;
+        expense.value = spanElemValue.textContent;
+
+        saveBtn.hidden = true;
+        editBtn.hidden = false;
+      } else {
+        alert("Wprowadź poprawne dane");
+      }
     });
 
     deleteBtn.addEventListener("click", (event) => {
       const target = event.target;
       const btnDiv = target.parentNode;
       const li = btnDiv.parentNode;
-      const itemToDelete = list.findIndex((object) => object.id == li.id);
-      totalExpenses -= list[itemToDelete].value;
-      list.splice(itemToDelete, 1);
+      const itemToDelete = listOfExpenses.findIndex(
+        (object) => object.id == li.id
+      );
+      totalExpenses -= listOfExpenses[itemToDelete].value;
+      listOfExpenses.splice(itemToDelete, 1);
       li.parentElement.removeChild(li);
       updateBalance();
       expensesSum.textContent = `${totalExpenses}`;
     });
 
-    list.push(expense);
+    listOfExpenses.push(expense);
     expenseName.value = "";
     expenseAmount.value = "";
     expensesSum.textContent = `${totalExpenses}`;
